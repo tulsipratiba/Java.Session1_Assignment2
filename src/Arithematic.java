d992a625c7978e529e90b3e65e15f6ae2d1e6830
tulsipratiba <sudhakartulsi@gmail.com>
2017-05-16 14:30:32 +0530

    3rd assignment

---
+++ src/Arithematic.java
@@ -0,0 +1,43 @@
+
+public class Arithematic 
+{
+public static void main(String[] args)
+	
+	{
+		
+		 int a;  
+	      a=20;  // declaring the value of a 
+	       
+	    System.out.println(" " +a);  // print the value of a 
+	    
+	    int b = a-- - --a;   
+	   {
+		   	System.out.println(" "+b); // print the value of b as 20-18
+		}
+	        
+	   int c = a--; 
+	   {
+	System.out.println(" "+c);  //print the value of c as 20
+	   }
+	   
+	        
+	   int d = a>>2; 
+	   {
+	System.out.println(" "+d); // print the value of d as 10
+	   }
+	    
+	        
+	  int e = a&b;
+	  {
+	 
+		System.out.println(" "+e); 
+		}
+	
+	   }
+}
+	 
+
+
+
+
+
